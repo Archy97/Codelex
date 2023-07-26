c414d5b7c42cdc36ebb3de39bd4cbd54da0a408e
Artjoms Malisevs <unikumav@gmail.com>
2023-07-26 13:34:20 +0200

    Exercise 6 done

---
+++ SumOfAllNumbers/SumOfAllNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SumOfAllNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var number = input.ToCharArray();
+            var sum = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum += int.Parse(number[i].ToString());
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+           
+        }
+    }
+}
