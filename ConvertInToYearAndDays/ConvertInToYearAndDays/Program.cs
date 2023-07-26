9040cd82c7bec9887ade6e58a620f8901406b951
Artjoms Malisevs <unikumav@gmail.com>
2023-07-26 14:37:04 +0200

    Exercise 8 done

---
+++ ConvertInToYearAndDays/ConvertInToYearAndDays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConvertMinutesToYearsAndDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of minutes: ");
+            var input = Console.ReadLine();
+            
+            if (int.TryParse(input, out int totalMinutes))
+            {
+                const int MinutesInDay = 24 * 60;
+                const int MinutesInYear = 365 * MinutesInDay;
+
+                int years = totalMinutes / MinutesInYear;
+                int remainingMinutes = totalMinutes % MinutesInYear;
+                int days = remainingMinutes / MinutesInDay;
+
+                Console.WriteLine($"{totalMinutes} minutes is approximately {years} years and {days} days.");
+            }
+                        Console.ReadKey();
+        }
+    }
+}
