61b272dee46d747bdcb9594039e1979ce0d7d62f
Artjoms Malisevs <unikumav@gmail.com>
2023-07-28 12:14:36 +0200

    Exercise 4 done

---
+++ Product1ToN/Product1ToN/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Product1ToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var sum = 1;
+
+            for (int number = 1; number <= 10; number++)
+            {
+                sum *= number;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
