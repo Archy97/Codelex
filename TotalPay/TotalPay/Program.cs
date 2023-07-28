d94134ab9d87a5a34de4e625a0b9bb66930ca755
Artjoms Malisevs <unikumav@gmail.com>
2023-07-28 16:50:07 +0200

    exercise 8 done

---
+++ TotalPay/TotalPay/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TotalPay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFinalPay(7.50, 35);
+            PrintFinalPay(8.20, 47);
+            PrintFinalPay(10.00, 73);
+        }
+
+        static void PrintFinalPay(double basePay, int hours)
+        {
+            double totalPay = basePay * hours + (basePay * 1.5) * (hours - 40);
+
+            if (basePay < 8.00) 
+            {
+                Console.WriteLine("Error: Base pay is below minimum wage.");
+            }
+            else if (hours > 60)
+            {
+                Console.WriteLine("Error: Hours worked exceed the maximum allowed.");
+            }
+            else
+            {
+                Console.WriteLine("Total Pay: " + totalPay);
+            }
+        }
+    }
+}
