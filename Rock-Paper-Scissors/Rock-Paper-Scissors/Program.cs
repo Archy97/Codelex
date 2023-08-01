316171cd16e15f9e7a5c63dadede815ade9f9173
Artjoms Malisevs <unikumav@gmail.com>
2023-08-01 22:58:27 +0200

    fixed

--- Rock-Paper-Scissors/Rock-Paper-Scissors/Program.cs
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/Program.cs
@@ -1,76 +1,48 @@
 ﻿using System;
 
-namespace Rock_Paper_Scissors
+namespace RockPaperScissors
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string playerChoice = Console.ReadLine();
+            string playerChoice = Console.ReadLine().ToLower(); // Convert to lowercase
             Random random = new Random();
             int computerChoice = random.Next(1, 4);
 
-            switch (computerChoice)
+            Console.WriteLine("Computer chose " + GetChoiceText(computerChoice));
+            Console.WriteLine("User chose " + playerChoice);
+
+            if (computerChoice == 1 && playerChoice == "rock" ||
+                computerChoice == 2 && playerChoice == "paper" ||
+                computerChoice == 3 && playerChoice == "scissors")
             {
-                case 1: 
-                    Console.WriteLine("Computer chose Rock");
+                Console.WriteLine("It is a tie.");
+            }
+            else if (computerChoice == 1 && playerChoice == "paper" ||
+                     computerChoice == 2 && playerChoice == "scissors" ||
+                     computerChoice == 3 && playerChoice == "rock")
+            {
+                Console.WriteLine("User wins");
+            }
+            else
+            {
+                Console.WriteLine("Computer wins");
+            }
+        }
 
-                    if (playerChoice == "rock")
-                    {
-                        Console.WriteLine("User chose Rock");
-                        Console.WriteLine("It is a tie.");
-                    }
-                    else if (playerChoice == "paper")
-                    {
-                        Console.WriteLine("User chose Paper");
-                        Console.WriteLine("User wins");
-                    }
-                    else if (playerChoice == "scissors")
-                    {
-                        Console.WriteLine("User chose Scissors");
-                        Console.WriteLine("Computer wins");
-                    }
-                    break;
-
-                case 2: 
-                    Console.WriteLine("Computer chose Paper");
-
-                    if (playerChoice == "rock")
-                    {
-                        Console.WriteLine("User chose Rock");
-                        Console.WriteLine("Computer wins");
-                    }
-                    else if (playerChoice == "paper")
-                    {
-                        Console.WriteLine("User chose Paper");
-                        Console.WriteLine("It is a tie.");
-                    }
-                    else if (playerChoice == "scissors")
-                    {
-                        Console.WriteLine("User chose Scissors");
-                        Console.WriteLine("User wins");
-                    }
-                    break;
-
-                case 3: 
-                    Console.WriteLine("Computer chose Scissors");
-
-                    if (playerChoice == "rock")
-                    {
-                        Console.WriteLine("User chose Rock");
-                        Console.WriteLine("User wins.");
-                    }
-                    else if (playerChoice == "paper")
-                    {
-                        Console.WriteLine("User chose Paper");
-                        Console.WriteLine("Computer wins");
-                    }
-                    else if (playerChoice == "scissors")
-                    {
-                        Console.WriteLine("User chose Scissors");
-                        Console.WriteLine("It is a tie.");
-                    }
-                    break;
+        static string GetChoiceText(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    return "Rock";
+                case 2:
+                    return "Paper";
+                case 3:
+                    return "Scissors";
+                default:
+                    return ("Invalid choice.");
             }
         }
     }
