b471581473dd25ada62877699830d7902aa420fa
Artjoms Malisevs <unikumav@gmail.com>
2023-08-01 22:13:27 +0200

    fixed

--- csharp-basics/exercises/FlowOfControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/LargestNumber/Program.cs
@@ -16,7 +16,7 @@ namespace LargestNumber
             Console.WriteLine("Input the 3rd number: ");
             var input3 =  int.Parse(Console.ReadLine());
 
-           Console.WriteLine(Math.Max(Math.Max(input1, input2), input3));
+            Console.WriteLine(Math.Max(Math.Max(input1, input2), input3));
         }
     }
 }
