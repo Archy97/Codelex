f81d83f2133df2a1640b9ed04323dbced495fdc7
Artjoms Malisevs <unikumav@gmail.com>
2023-08-01 17:49:32 +0200

    Exercise 6 done.

---
+++ GetTheCentury/GetTheCentury/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GetTheCentury
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            int century = GetCentury(year);
+            Console.WriteLine("The year " + year + " belongs to the " + century + " century.");
+        }
+
+        public static int GetCentury(int year)
+        {
+            int century = (year - 1) / 100 + 1;
+
+            return century;
+        }
+    }
+}
