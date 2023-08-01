2732661f41557ea433c6533819174003e28b0c0b
Artjoms Malisevs <unikumav@gmail.com>
2023-08-01 22:16:17 +0200

    Fixed

--- csharp-basics/exercises/FlowOfControl/PositiveNegativeNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/PositiveNegativeNumber/Program.cs
@@ -8,18 +8,16 @@ namespace PositiveNegativeNumber
         {
             Console.WriteLine("Enter the number.");
             var input = int.Parse(Console.ReadLine());
-            
+
             if (input > 0)
             {
                 Console.WriteLine("Number is positive");
-            } 
-
-            else if ( input < 0) 
+            }
+            else if (input < 0)
             {
                 Console.WriteLine("Number is negative");
-            } 
-
-            else 
+            }
+            else
             {
                 Console.WriteLine("Number is zero");
             }
