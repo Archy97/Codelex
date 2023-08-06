2b706a273e7c5bf8f61538f3010f51f8c7e66971
Artjoms Malisevs <unikumav@gmail.com>
2023-08-06 15:02:54 +0200

    Exercise 11 done

---
+++ Exercise 11/Exercise 11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindNemoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNemo("I am finding Nemo !"));
+            Console.WriteLine(FindNemo("Nemo is me"));
+            Console.WriteLine(FindNemo("I Nemo am"));
+        }
+
+        static string FindNemo(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == "Nemo")
+                {
+                    return $"I found Nemo at {i + 1}!";
+                }
+            }
+
+            return "Nemo not found :(";
+        }
+    }
+}
