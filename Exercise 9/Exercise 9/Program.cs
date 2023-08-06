be1622b5e5ada77a2b429f8f57cc1e714f8b78bc
Artjoms Malisevs <unikumav@gmail.com>
2023-08-06 14:17:32 +0200

    Exe 9 done

---
+++ Exercise 9/Exercise 9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "mavis", "senaida", "letty" };
+            var transformedStrings = CapMe(inputArray);
+            string concatenatedResult = string.Join(Environment.NewLine, transformedStrings);
+            Console.WriteLine(concatenatedResult);
+        }
+
+        static string[] CapMe(string[] input)
+        {
+            var transformedStrings = input.Select(str =>
+                char.ToUpper(str[0]) + str.Substring(1).ToLower());
+
+            return transformedStrings.ToArray();
+        }
+    }
+}
