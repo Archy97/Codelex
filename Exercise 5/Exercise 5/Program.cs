1d2cd9511dbcc289e7d1693a6d898f543d1c80e3
Artjoms Malisevs <unikumav@gmail.com>
2023-08-07 14:08:10 +0200

    Exe 5 done

---
+++ Exercise 5/Exercise 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace Exercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine("Enter first word: ");
+            var input1 = Console.ReadLine();
+            Console.WriteLine("Enter second word");
+            var input2 = Console.ReadLine();
+
+            int remainingSpace = 30 - input1.Length - input2.Length;
+
+            Console.Write(input1);
+
+            for (int i = 0; i < remainingSpace; i++)
+            {
+                Console.Write(".");
+            }
+
+            Console.WriteLine(input2);
+        }
+    }
+}
