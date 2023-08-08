06a1dbfdfffc840be97378adaa8c6186d9c03aea
Artjoms Malisevs <unikumav@gmail.com>
2023-08-08 21:46:43 +0200

    Exercise 9 done

---
+++ Exercise 9/Exercise 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RandomNumberExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Desired sum: ");
+            int input = int.Parse(Console.ReadLine());
+            Random random = new Random();
+
+            int sum, randomNumber1, randomNumber2;
+
+            do
+            {
+                randomNumber1 = random.Next(1, 7);
+                randomNumber2 = random.Next(1, 7);
+                sum = randomNumber1 + randomNumber2;
+
+                Console.WriteLine($" {randomNumber1} and  {randomNumber2} = {sum}");
+
+            } while (sum != input);
+        }
+    }
+}
