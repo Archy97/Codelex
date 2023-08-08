7f16411d0cba387561c92624df784ad487e31791
Artjoms Malisevs <unikumav@gmail.com>
2023-08-08 21:16:43 +0200

    Exercise 8 done

---
+++ Exercise #8/Exercise #8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 4;
+            int input = int.Parse(Console.ReadLine());
+            int totalLength = (input - 1) * length;
+
+            for (int i = 0; i < input; i++)
+            {
+                int strLength = i * length;
+
+                Console.WriteLine(new string('/', totalLength - strLength) + new string('*', strLength)
+                                    + new string('*', strLength) + new string('\\', totalLength - strLength));
+            }
+        }
+    }
+}
