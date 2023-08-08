7f098b51c43f1125460d225cd806721c7e4337b6
Artjoms Malisevs <unikumav@gmail.com>
2023-08-08 14:53:19 +0200

    Fixed

--- csharp-basics/exercises/Arrays/Exercise4/Program.cs
+++ csharp-basics/exercises/Arrays/Exercise4/Program.cs
@@ -1,34 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Exercise4
+class Solution
 {
-    class Program
+    static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        List<int> numbers = new List<int>()
         {
-            int[] myArray =
-            {
-                1789, 2035, 1899, 1456, 2013,
-                1458, 2458, 1254, 1472, 2365,
-                1456, 2265, 1457, 2456
-            };
+            1789, 2035, 1899, 1456, 2013,
+            1458, 2458, 1254, 1472, 2365,
+            1456, 2265, 1457, 2456
+        };
 
-            Console.Write("Enter a number to check: ");
-            int inputNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter a number to check: ");
+        int inputNumber = int.Parse(Console.ReadLine());
 
-            bool found = false;
-
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                if (myArray[i] == inputNumber)
-                {
-                    Console.WriteLine("Contains");
-                }
-                else
-                {
-                    Console.WriteLine("Not found");
-                }
-            }
+        if (numbers.Contains(inputNumber))
+        {
+            Console.WriteLine("Contains");
+        }
+        else
+        {
+            Console.WriteLine("Not found");
         }
     }
 }
