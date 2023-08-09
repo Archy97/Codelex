21014f6ca7378081810b95ee96bbc3b7c13c3b74
Artjoms Malisevs <unikumav@gmail.com>
2023-08-09 13:41:59 +0200

    Exercise 1 done

---
+++ Exercise 1/Exercise 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Shop;
+using System;
+
+namespace Shop
+{
+    public class Product
+    {
+        public string Name { get; }
+        public double PriceAtStart { get; }
+        public int AmountAtStart { get; }
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            Name = name;
+            PriceAtStart = priceAtStart;
+            AmountAtStart = amountAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{Name},  {PriceAtStart}, EUR {AmountAtStart} units");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product mouse = new Product("Logitech mouse", 70.00, 14);
+        Product iphone = new Product("iPhone 5s", 999.99, 3);
+        Product projector = new Product("Epson EB-U05", 440.46, 1);
+
+        mouse.PrintProduct();
+        iphone.PrintProduct();
+        projector.PrintProduct();
+    }
+}
