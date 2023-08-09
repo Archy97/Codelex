7d0e5bef3b043526ec1e418488882df3da802840
Artjoms Malisevs <unikumav@gmail.com>
2023-08-09 12:49:15 +0200

    Exercise 11 done

---
+++ Exercise 11/Exercise 11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CharCaseSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            char[] charArray = input.ToCharArray();
+            char[] charSwappedChars = new char[charArray.Length];
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                charSwappedChars[i] = char.IsUpper(charArray[i]) ? char.ToLower(charArray[i]) : char.ToUpper(charArray[i]);
+            }
+
+            string swappedString = new string(charSwappedChars);
+            Console.WriteLine(swappedString);
+        }
+    }
+}
