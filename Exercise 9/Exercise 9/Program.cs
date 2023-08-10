bd5a7ae90a1bf47c80d9acfa99990539ad072731
Artjoms Malisevs <unikumav@gmail.com>
2023-08-10 11:14:58 +0200

    fixed

--- Exercise 9/Exercise 9/Program.cs
+++ Exercise 9/Exercise 9/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
- Loops
 
 namespace RandomNumberExample
 {
@@ -19,31 +18,9 @@ namespace RandomNumberExample
                 randomNumber2 = random.Next(1, 7);
                 sum = randomNumber1 + randomNumber2;
 
-                Console.WriteLine($" {randomNumber1} and  {randomNumber2} = {sum}");
+                Console.WriteLine($" {randomNumber1} and {randomNumber2} = {sum}");
 
             } while (sum != input);
-=======
-using System.Linq;
-
-namespace Exercise_9
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string[] inputArray = { "mavis", "senaida", "letty" };
-            var transformedStrings = CapMe(inputArray);
-            string concatenatedResult = string.Join(Environment.NewLine, transformedStrings);
-            Console.WriteLine(concatenatedResult);
-        }
-
-        static string[] CapMe(string[] input)
-        {
-            var transformedStrings = input.Select(str =>
-                char.ToUpper(str[0]) + str.Substring(1).ToLower());
-
-            return transformedStrings.ToArray();
- main
         }
     }
 }
