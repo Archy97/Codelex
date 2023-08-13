64b3fd9bef26935c40d3cdfabd2ea51085341dde
Artjoms Malisevs <unikumav@gmail.com>
2023-08-13 21:04:08 +0200

    fix

--- Exercise #8/Exercise #8/Program.cs
+++ Exercise #8/Exercise #8/Program.cs
@@ -7,14 +7,15 @@ namespace Exercise_8
         static void Main(string[] args)
         {
             int input = int.Parse(Console.ReadLine());
-            int totalLength = input * 4 - 4; 
+            int totalLength = input * 4 - 4;
 
             for (int i = 0; i < input; i++)
             {
                 int strLength = i * 4;
+                int spaceLength = totalLength - strLength;
 
-                Console.WriteLine(new string('/', totalLength - strLength) + new string('*', strLength)
-                                    + new string('*', strLength) + new string('\\', totalLength - strLength));
+                Console.WriteLine(new string('/', spaceLength) + new string('*', strLength)
+                                                               + new string('*', strLength) + new string('\\', spaceLength));
             }
         }
     }
