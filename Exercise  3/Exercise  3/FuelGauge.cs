31e56d0d6c430812bbb8c681c1a6f86fe2a85fcc
Artjoms Malisevs <unikumav@gmail.com>
2023-08-14 00:41:21 +0200

    Exe 3 done

---
+++ Exercise  3/Exercise  3/FuelGauge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise__3
+{
+    internal class FuelGauge
+    {
+        public int CurrentFuelLiters;
+
+        public FuelGauge()
+        {
+            CurrentFuelLiters = 0;
+        }
+
+        public int ReportCurrentLiters()
+        {
+            return CurrentFuelLiters;
+        }
+
+        public void FillUp(int amount)
+        {
+            CurrentFuelLiters += amount;
+        }
+
+        public int DecrementFuel()
+        {
+            return CurrentFuelLiters--;
+        }
+    }
+}
---
+++ Exercise  3/Exercise  3/Odometer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise__3
+{
+    internal class Odometer
+    {
+        public int CurrentMileage;
+        public FuelGauge fuelGauge;
+        public int Count;
+
+        public Odometer(FuelGauge fuelgauge, int CurrentMileage)
+        {
+            fuelGauge = fuelgauge;
+            CurrentMileage = 0;
+        }
+
+        public int ReportCurrentMileage()
+        {
+            return CurrentMileage;
+        }
+
+        public void IncrementMileage()
+        {
+            if (CurrentMileage == 9999999)
+            {
+                CurrentMileage = 0;
+            }
+            else
+            {
+                CurrentMileage++;
+                Count++;
+            }
+        }
+
+        public void Decrement()
+        {
+            if (Count == 10)
+            {
+                fuelGauge.DecrementFuel();
+                Count = 0;
+            }
+        }
+    }
+}
