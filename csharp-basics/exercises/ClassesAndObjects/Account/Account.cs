1762181ad57d6633039b160e44bb9902d33d2875
Artjoms Malisevs <unikumav@gmail.com>
2023-08-13 20:58:16 +0200

    Exe 11 done.

--- csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -1,7 +1,31 @@
-﻿namespace Account
+﻿using System;
+
+namespace Account
 {
     class Account
     {
-        
+        public string BankAccount;
+        public double Balance;
+
+        public Account(string bankAccount, double balance)
+        {
+            BankAccount = bankAccount;
+            Balance = balance;
+        }
+
+        public double Withdraw(double withdraw)
+        {
+            return Balance -= withdraw;
+        }
+
+        public double Deposit(double deposit)
+        {
+            return Balance += deposit;
+        }
+
+        public override string ToString()
+        {
+            return $"{BankAccount}: {Balance:C}";
+        }
     }
 }
