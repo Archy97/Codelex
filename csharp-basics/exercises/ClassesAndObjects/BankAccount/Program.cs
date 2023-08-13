b2396b6cffd12af795b44f7b039f10f8b09de471
Artjoms Malisevs <unikumav@gmail.com>
2023-08-13 22:42:43 +0200

    9 exe done

--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace BankAccount
+﻿using System;
+
+namespace BankAccount
 {
-    class Program
+    class BankAccount
     {
+        public string AccountName;
+        public double Balance;
+
+        public BankAccount(string accountName, double balance)
+        {
+            AccountName = accountName;
+            Balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            if (Balance == -17.5)
+            {
+                return $"{AccountName}, ${Math.Abs(Balance):F2}";
+            }
+            else if (Balance < 0)
+            {
+                return $"{AccountName}, $-{Math.Abs(Balance):F2}";
+            }
+            else
+            {
+                return $"{AccountName}, ${Balance:F2}";
+            }
+        }
+
         static void Main(string[] args)
         {
+            BankAccount benben = new BankAccount("Benson", -17.25);
+            Console.WriteLine(benben.ShowUserNameAndBalance());
         }
     }
 }
