b52c98c190ee07f339b7914237f3b43a4255ec05
Artjoms Malisevs <unikumav@gmail.com>
2023-08-14 20:42:02 +0200

    Exercise 10 done

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
@@ -1,7 +1,49 @@
-﻿namespace VideoStore
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VideoStore
 {
     class Video
     {
+        private List<double> _ratings;
 
+        public string Title { get; set; }
+        public bool IsAvailable { get; set; }
+        public int LikedCount { get; private set; }
+
+        public Video(string title)
+        {
+            Title = title;
+            IsAvailable = true;
+            _ratings = new List<double>();
+             LikedCount = 0;
+        }
+
+        public double AverageRating => _ratings.Any() ? _ratings.Average() : 0;
+
+        public void BeingCheckedOut()
+        {
+            IsAvailable = false;
+        }
+
+        public void BeingReturned()
+        {
+            IsAvailable = true;
+        }
+
+        public void ReceiveRating(double rating)
+        {
+            _ratings.Add(rating);
+            if (rating >= 7)
+            {
+                LikedCount++;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Title}, rating: {AverageRating}, IsAvailable {IsAvailable}";
+        }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -1,8 +1,82 @@
-﻿namespace VideoStore
+﻿using System;
+using System.Collections.Generic;
+
+namespace VideoStore
 {
     class VideoStore
     {
+        private List<Video> _inventory;
+
+        public VideoStore()
+        {
+            _inventory = new List<Video>();
+        }
+
+        public void AddVideo(string title)
+        {
+            _inventory.Add(new Video(title));
+        }
+
+        public void CheckOut(string title)
+        {
+            for (int i = 0; i < _inventory.Count; i++)
+            {
+                if (_inventory[i].Title == title)
+                {
+                    _inventory[i].BeingCheckedOut();
+                    break;
+                }
+            }
+        }
+
+        public void ReturnVid(string title)
+        {
+            for (int i = 0; i < _inventory.Count; i++)
+            {
+                if (_inventory[i].Title == title)
+                {
+                    _inventory[i].BeingReturned();
+                    break;
+                }
+            }
+        }
+
+        public void ReceiveRating(string title, double rating)
+        {
+            for (int i = 0; i < _inventory.Count; i++)
+            {
+                if (_inventory[i].Title == title)
+                {
+                    _inventory[i].ReceiveRating(rating);
+                    break;
 
 
+                }
+                
+            }
+        }
+        public void DisplayLikedPercentage(string title)
+        {   
+            foreach (var video in _inventory)
+            {
+                if (video.Title == title)
+                {
+                    double likedPercentage = (double)video.LikedCount / _inventory.Count * 100;
+                    Console.WriteLine($"Percentage of users who liked '{title}': {likedPercentage}%");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Video '{title}' not found in inventory.");
+        }
+
+        public void ListInventory()
+        {
+            for (int i = 0; i < _inventory.Count; i++)
+            {
+                var video = _inventory[i];
+                Console.WriteLine(video);
+            }
+        }
     }
 }
