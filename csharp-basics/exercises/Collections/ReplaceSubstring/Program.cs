5825373b5d78052082e367293f227782459b8b76
Artjoms Malisevs <unikumav@gmail.com>
2023-08-16 20:52:37 +0200

    LinQ Replace substring Done

--- csharp-basics/exercises/Collections/ReplaceSubstring/Program.cs
+++ csharp-basics/exercises/Collections/ReplaceSubstring/Program.cs
@@ -1,12 +1,17 @@
-﻿namespace ReplaceSubstring
+﻿using System;
+using System.Linq;
+
+namespace ReplaceSubstring
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var words = new[] { "near", "speak", "tonight", "weapon", "customer", "deal", "lawyer" };
-            //ToDo: Write a query that replaces 'ea' substring with astersik (*) in given list of words.
-            //ToDo: "learn", "current", "deal" →  "l*rn", "current", "d*l"
+
+            var transformed = words.Select(word => word.Replace("ea", "*")).ToArray();
+
+            Console.WriteLine(string.Join(" ", transformed));
         }
     }
 }
