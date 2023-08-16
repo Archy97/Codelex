fb21e10b164e69e7f1f75aac14dc92c254b3c581
Artjoms Malisevs <unikumav@gmail.com>
2023-08-16 18:42:03 +0200

    Exe DaysName done

--- csharp-basics/exercises/Collections/DaysNames/Program.cs
+++ csharp-basics/exercises/Collections/DaysNames/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-//ToDo: Write a query that returns names of days
-//https://docs.microsoft.com/en-us/dotnet/api/system.dayofweek?view=net-6.0
+﻿using System;
+
+public enum DaysName
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+class Program
+{
+    static void Main()
+    {
+        foreach (DaysName day in Enum.GetValues(typeof(DaysName)))
+        {
+            Console.WriteLine(day);
+        }
+    }
+}
