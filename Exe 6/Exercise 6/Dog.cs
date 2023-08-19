e8c85297ab778c850c9ea5e183c5685f07d0cf76
Artjoms Malisevs <unikumav@gmail.com>
2023-08-19 10:33:48 +0200

    Fixed 6 Exe

---
+++ Exe 6/Exercise 6/Dog.cs
@@ -0,0 +1,23 @@
+﻿public class Dog
+{
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public Dog Mother;
+    public Dog Father;
+
+    public Dog(string name, string gender)
+    {
+        Name = name;
+        Gender = gender;
+    }
+
+    public string FathersName()
+    {
+        return (Father != null) ? Father.Name : "Unknown";
+    }
+
+    public bool HasSameMotherAs(Dog otherDog)
+    {
+        return Mother.Name == otherDog.Mother.Name;
+    }
+}
