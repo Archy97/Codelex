b2cf8293b76dfee0b12c92e1292c5bd16939264b
Artjoms Malisevs <unikumav@gmail.com>
2023-08-19 12:12:48 +0200

    Fixed exe 1

---
+++ Exercise 1/Exercise 1/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Shop
+{
+    public class Product
+    {
+        private string _name { get; }
+        private double _priceAtStart { get; }
+        private int _amountAtStart { get; }
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            _name = name;
+            _priceAtStart = priceAtStart;
+            _amountAtStart = amountAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name}, {_priceAtStart}, EUR {_amountAtStart} units");
+        }
+    }
+}
--- Exercise 1/Exercise 1/Program.cs
+++ Exercise 1/Exercise 1/Program.cs
@@ -1,38 +1,18 @@
-﻿using Shop;
-using System;
+﻿using System;
 
 namespace Shop
 {
-    public class Product
+    class Program
     {
-        public string Name { get; }
-        public double PriceAtStart { get; }
-        public int AmountAtStart { get; }
-
-        public Product(string name, double priceAtStart, int amountAtStart)
+        static void Main(string[] args)
         {
-            Name = name;
-            PriceAtStart = priceAtStart;
-            AmountAtStart = amountAtStart;
+            Product mouse = new Product("Logitech mouse", 70.00, 14);
+            Product iphone = new Product("iPhone 5s", 999.99, 3);
+            Product projector = new Product("Epson EB-U05", 440.46, 1);
+
+            mouse.PrintProduct();
+            iphone.PrintProduct();
+            projector.PrintProduct();
         }
-
-        public void PrintProduct()
-        {
-            Console.WriteLine($"{Name},  {PriceAtStart}, EUR {AmountAtStart} units");
-        }
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Product mouse = new Product("Logitech mouse", 70.00, 14);
-        Product iphone = new Product("iPhone 5s", 999.99, 3);
-        Product projector = new Product("Epson EB-U05", 440.46, 1);
-
-        mouse.PrintProduct();
-        iphone.PrintProduct();
-        projector.PrintProduct();
     }
 }
