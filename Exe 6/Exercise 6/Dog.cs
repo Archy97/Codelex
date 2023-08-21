f15fd5b1e1b61f91467a145ac3f4ad03baf51885
Artjoms Malisevs <unikumav@gmail.com>
2023-08-21 10:08:57 +0200

    fixed exe 6

--- Exe 6/Exercise 6/Dog.cs
+++ Exe 6/Exercise 6/Dog.cs
@@ -1,23 +1,23 @@
 ﻿public class Dog
 {
-    public string Name { get; set; }
-    public string Gender { get; set; }
-    public Dog Mother;
-    public Dog Father;
+    private string _name;
+    private string _gender;
+    public Dog Mother { get; set; }
+    public Dog Father { get; set; }
 
     public Dog(string name, string gender)
     {
-        Name = name;
-        Gender = gender;
+        _name = name;
+        _gender = gender;
     }
 
     public string FathersName()
     {
-        return (Father != null) ? Father.Name : "Unknown";
+        return (Father != null) ? Father.FathersName() : "Unknown";
     }
 
     public bool HasSameMotherAs(Dog otherDog)
     {
-        return Mother.Name == otherDog.Mother.Name;
+        return Mother?._name == otherDog.Mother?._name;
     }
 }
