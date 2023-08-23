efbf8081aedd0c9d97493e81617b4f73f57212ab
Artjoms Malisevs <unikumav@gmail.com>
2023-08-23 21:07:52 +0200

    Dayname fix

--- csharp-basics/exercises/Collections/DaysNames/Program.cs
+++ csharp-basics/exercises/Collections/DaysNames/Program.cs
@@ -1,21 +1,10 @@
 ﻿using System;
 
-public enum DaysName
-{
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
-}
-
 class Program
 {
     static void Main()
     {
-        foreach (DaysName day in Enum.GetValues(typeof(DaysName)))
+        foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
         {
             Console.WriteLine(day);
         }
